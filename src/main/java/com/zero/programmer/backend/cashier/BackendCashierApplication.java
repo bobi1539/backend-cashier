9ec833fa38f85bd20bf4097194b7728213c51ca8
bobi.rival <bobi.rival@dwidasa.com>
2022-07-12 14:13:21 +0700

    Set up project

---
+++ src/main/java/com/zero/programmer/backend/cashier/BackendCashierApplication.java
@@ -0,0 +1,13 @@
+package com.zero.programmer.backend.cashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCashierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCashierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/programmer/backend/cashier/BackendCashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.programmer.backend.cashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCashierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
